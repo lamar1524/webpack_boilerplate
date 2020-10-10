665db2c10567d24572ce9e281ddf6f0e18eec0da
Paweł <pawel.wybrzeze@gmail.com>
2020-10-10 20:14:15 +0200

    Initial commit for react boilerplate

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.scss';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.render(<div>Halko</div>, document.getElementById('root'));
---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+import { getMocks, sliceMocks } from './index.js';
+
+// getMocks() test
+test('Checking if results mocks are being generated properly', () => {
+  expect(getMocks().length).toBe(150);
+});
+// sliceMocks() test
+test('Checking if results mocks are being sliced properly', () => {
+  expect(sliceMocks(getMocks(),1).length).toBe(9);
+  // last page is not divisible by 9, so it must be shorter. In this case it's 6
+  expect(sliceMocks(getMocks(), 17).length).toBe(6);
+});
